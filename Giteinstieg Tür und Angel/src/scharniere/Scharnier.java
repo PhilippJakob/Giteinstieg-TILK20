2c2de4cfe3f4c486ea49bdd61bdb3b803d360ec9
piel.florian_martin <piel.florian_martin@PC230116.osz.intern>
2019-02-13 10:57:13 +0100

    010

--- Giteinstieg Tür und Angel/src/scharniere/Scharnier.java
+++ Giteinstieg Tür und Angel/src/scharniere/Scharnier.java
@@ -5,14 +5,16 @@ public class Scharnier
 
 		public void bohren()
 	{
-		System.out.println("");
+		
 	}
 
 
 
 
 	public void anschraubenAnTür()
-	{}
+	{
+		System.out.println("");
+	}
 
 
 	public void anschraubenAmTürrahmen()
