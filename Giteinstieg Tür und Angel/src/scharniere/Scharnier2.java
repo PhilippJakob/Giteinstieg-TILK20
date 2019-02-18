ee6d58831c6b85e57e710a0b5037b4c50f6f4885
ye.yilai <ye.yilai@PC230114.osz.intern>
2019-02-18 10:00:17 +0000

    Ich spuck in die Suppe.

---
+++ Giteinstieg Tür und Angel/src/scharniere/Scharnier2.java
@@ -0,0 +1,23 @@
+package scharniere;
+//was rein schreiben keine ahnung
+
+public class Scharnier2
+{
+
+		public void bohren()
+	{
+		
+	}
+
+
+
+
+	public void anschraubenAnTür()
+	{}
+
+
+	public void anschraubenAmTürrahmen()
+	{}
+
+
+}
