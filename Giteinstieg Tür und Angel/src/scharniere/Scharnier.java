706b895cd3af985b4a607daf2f4bffcd8d0545cf
herold.dominic <herold.dominic@PC230106.osz.intern>
2019-02-20 07:58:05 +0100

    Fehler behoben vom Mergen

--- Giteinstieg Tür und Angel/src/scharniere/Scharnier.java
+++ Giteinstieg Tür und Angel/src/scharniere/Scharnier.java
@@ -3,15 +3,8 @@ package scharniere;
 public class Scharnier
 {
 
-		public void bohren()
-	{
-		System.out.println("asd");
-
-		System.out.println("Schanier schnell bohren");
-
-		System.out.println("Rawad ist cool :)");
-	}
-
+	public void bohren()
+	{}
 
 
 	public void anschraubenAnTür()
